a30d01da62f0dc63c3be213a469abe277177cd37
nency <narolanency@gmail.com>
2023-12-28 17:43:15 +0530

    header responsive change

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
 import main from '/styles/Main.module.scss'
 import Link from 'next/link'
 const Navbar1 = () => {
+    const [expanded, setExpanded] = useState(false);
 
+    const handleNavItemClick = () => {
+      setExpanded(false);
+    };
     return (
         <>
             <div className='fixed-top'>
                 <div className=" container-fluid p-0">
                     <div className={main.headerdiv}>
                         <div className="container">
-                            <Navbar expand="lg" className="p-4 p-lg-0 ">
-                                <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-white shadow-none' />
-                                <Navbar.Collapse id="basic-navbar-nav" className='d-flex justify-content-center'>
-                                    <Nav>
-                                        <Link href='/' legacyBehavior className='p-0' ><a className={main.navlinkdiv}><li>Home</li></a></Link>
-                                        <Link href="/about" legacyBehavior className='p-0' ><a className={main.navlinkdiv}><li>About</li></a></Link>
-                                        <Link href="/project" legacyBehavior className='p-0' ><a className={main.navlinkdiv}><li>Project</li></a></Link>
-                                        <Link href="/service" legacyBehavior className='p-0' ><a className={main.navlinkdiv}><li>Service</li></a></Link>
-                                        <Link href="/blog" legacyBehavior className='p-0' ><a className={main.navlinkdiv}><li>Blog</li></a></Link>
-                                        <Link href="/contact-us" legacyBehavior className='p-0' ><a className={main.navlinkdiv}><li>Contact Us</li></a></Link>
+                            <Navbar expand="lg" className="p-4 p-lg-0 " expanded={expanded}>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-white shadow-none'   onClick={() => setExpanded(!expanded)}/>
+                                 <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav  onSelect={() => setExpanded(false)}>
+                                        <Link href='/' legacyBehavior className='p-0' passHref><a className={main.navlinkdiv}  onClick={handleNavItemClick}><li>Home</li></a></Link>
+                                        <Link href="/about" legacyBehavior className='p-0' passHref><a className={main.navlinkdiv}  onClick={handleNavItemClick}><li>About</li></a></Link>
+                                        <Link href="/project" legacyBehavior className='p-0' passHref><a className={main.navlinkdiv}  onClick={handleNavItemClick}><li>Project</li></a></Link>
+                                        <Link href="/service" legacyBehavior className='p-0' passHref><a className={main.navlinkdiv}  onClick={handleNavItemClick}><li>Service</li></a></Link>
+                                        <Link href="/blog" legacyBehavior className='p-0' passHref><a className={main.navlinkdiv}  onClick={handleNavItemClick}><li>Blog</li></a></Link>
+                                        <Link href="/contact-us" legacyBehavior className='p-0' passHref><a className={main.navlinkdiv}  onClick={handleNavItemClick}><li>Contact Us</li></a></Link>
                                     </Nav>
                                 </Navbar.Collapse>
                             </Navbar>
